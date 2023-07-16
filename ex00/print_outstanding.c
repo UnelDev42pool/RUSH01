6d8372733d8fc6db3b1f070640648f0f54e5e3ca
UnelDev <enee.prog@gmail.com>
2023-07-16 14:26:55 +0200

    add print three

--- ex00/print_outstanding.c
+++ ex00/print_outstanding.c
@@ -6,7 +6,7 @@
 /*   By: edi-iori <edi-iori@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 10:30:28 by edi-iori          #+#    #+#             */
-/*   Updated: 2023/07/16 12:16:08 by edi-iori         ###   ########lyon.fr   */
+/*   Updated: 2023/07/16 14:26:40 by edi-iori         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	print_array(int **array);
 // /!\/!\|
 
-int	print_four_column_hight(int i, int **final)
+int	print_four_hight(int i, int **final)
 {
 	if ((final[0][i] == 0 || final[0][i] == 1)
 		&& (final[1][i] == 0 || final[1][i] == 2)
@@ -32,7 +32,7 @@ int	print_four_column_hight(int i, int **final)
 	return (0);
 }
 
-int print_one_column_hight(int i, int **final)
+int print_one_hight(int i, int **final)
 {
 	if ((final[0][i] == 0 || final[0][i] == 4))
 	{
@@ -43,9 +43,20 @@ int print_one_column_hight(int i, int **final)
 	return (0);
 }
 
+int print_three_hight(int i, int **final)
+{
+	if ((final[2][i] == 0 || final[2][i] == 4))
+	{
+		final[2][i] = 4;
+	}
+	else
+		return (1);
+	return (0);
+}
+
 ///////////////////////////////////////////////////////////
 
-int	print_four_column_low(int i, int **final)
+int	print_four_low(int i, int **final)
 {
 	if ((final[3][i] == 0 || final[3][i] == 1)
 		&& (final[2][i] == 0 || final[2][i] == 2)
@@ -62,7 +73,7 @@ int	print_four_column_low(int i, int **final)
 	return (0);
 }
 
-int print_one_column_low(int i, int **final)
+int print_one_low(int i, int **final)
 {
 	if ((final[3][i] == 0 || final[3][i] == 4))
 	{
@@ -73,9 +84,20 @@ int print_one_column_low(int i, int **final)
 	return (0);
 }
 
+int print_three_low(int i, int **final)
+{
+	if ((final[1][i] == 0 || final[1][i] == 4))
+	{
+		final[1][i] = 4;
+	}
+	else
+		return (1);
+	return (0);
+}
+
 ///////////////////////////////////////////////////////////
 
-int	print_four_ligne_right(int i, int **final)
+int	print_four_right(int i, int **final)
 {
 	if ((final[i][0] == 0 || final[i][0] == 1)
 		&& (final[i][1] == 0 || final[i][1] == 2)
@@ -92,7 +114,7 @@ int	print_four_ligne_right(int i, int **final)
 	return (0);
 }
 
-int print_one_ligne_right(int i, int **final)
+int print_one_right(int i, int **final)
 {
 	if ((final[i][4] == 0 || final[i][4] == 4))
 	{
@@ -103,9 +125,20 @@ int print_one_ligne_right(int i, int **final)
 	return (0);
 }
 
+int print_three_right(int i, int **final)
+{
+	if ((final[i][2] == 0 || final[i][2] == 4))
+	{
+		final[i][2] = 4;
+	}
+	else
+		return (1);
+	return (0);
+}
+
 ///////////////////////////////////////////////////////////
 
-int	print_four_ligne_left(int i, int **final)
+int	print_four_left(int i, int **final)
 {
 	if ((final[i][0] == 0 || final[i][0] == 1)
 		&& (final[i][1] == 0 || final[i][1] == 2)
@@ -122,7 +155,7 @@ int	print_four_ligne_left(int i, int **final)
 	return (0);
 }
 
-int print_one_ligne_left(int i, int **final)
+int print_one_left(int i, int **final)
 {
 	if ((final[i][4] == 0 || final[i][4] == 4))
 	{
@@ -133,6 +166,16 @@ int print_one_ligne_left(int i, int **final)
 	return (0);
 }
 
+int print_three_left(int i, int **final)
+{
+	if ((final[i][2] == 0 || final[i][2] == 4))
+	{
+		final[i][2] = 4;
+	}
+	else
+		return (1);
+	return (0);
+}
 ///////////////////////////////////////////////////////////
 
 
@@ -143,48 +186,56 @@ int	print_outstanding(int **final, int **give)
 	i = 0;
 	while (i < 4)
 	{
-		give[0][i] = give[0][i];
 		if (give[0][i] == 4)
-			if(print_four_column_hight(i, final))
+			if(print_four_hight(i, final))
+				return (1);
+		if (give[0][i] == 3)
+			if(print_three_hight(i, final))
 				return (1);
 		if (give[0][i] == 1)
-			if(print_one_column_hight(i, final))
+			if(print_one_hight(i, final))
 				return (1);
 		i++;
 	}
 	i = 0;
 	while (i < 4)
 	{
-		give[1][i] = give[1][i];
 		if (give[1][i] == 4)
-			if(print_four_ligne_right(i, final))
+			if(print_four_right(i, final))
+				return (1);
+		if (give[1][i] == 3)
+			if(print_three_right(i, final))
 				return (1);
 		if (give[1][i] == 1)
-			if(print_one_ligne_right(i, final))
+			if(print_one_right(i, final))
 				return (1);
 		i++;
 	}
 	i = 0;
 	while (i < 4)
 	{
-		give[2][i] = give[2][i];
 		if (give[2][i] == 4)
-			if(print_four_ligne_left(i, final))
+			if(print_four_left(i, final))
+				return (1);
+		if (give[2][i] == 3)
+			if(print_three_left(i, final))
 				return (1);
 		if (give[2][i] == 1)
-			if(print_one_ligne_left(i, final))
+			if(print_one_left(i, final))
 				return (1);
 		i++;
 	}
 	i = 0;
 	while (i < 4)
 	{
-		give[3][i] = give[3][i];
 		if (give[3][i] == 4)
-			if(print_four_column_low(i, final))
+			if(print_four_low(i, final))
+				return (1);
+		if (give[3][i] == 3)
+			if(print_three_low(i, final))
 				return (1);
 		if (give[3][i] == 1)
-			if(print_one_column_low(i, final))
+			if(print_one_low(i, final))
 				return (1);
 		i++;
 	}
